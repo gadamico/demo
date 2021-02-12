7f303614b71d98fac00574e9c9f8bc56496d1e02
Greg Damico <greg.damico@flatironschool.com>
2021-02-11 18:06:41 -0800

    add animal class

---
+++ src/example.py
@@ -0,0 +1,11 @@
+# This file is a sample.
+
+import seaborn as sns
+import pandas as import pd
+
+def Animal():
+    def __init__(self, legs, wings):
+        self.legs = legs
+        self.wings = wings
+    
+
